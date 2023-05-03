9e009666a06c0dc42f14985ac39e8375c0857331
Achinta <achintadutta1997@gmail.com>
2023-05-04 03:42:24 +0530

    first commit

---
+++ src/textBlock.js
@@ -0,0 +1,28 @@
+import "./App.css";
+
+function TextBlock(){
+    return (
+        <div id="textblock">
+        <div id="textblock-container">
+            <h1 id="textblock-title">Parallax</h1>
+            <p id="textblock-content">
+            Parallax, in astronomy, the difference in direction of a celestial object as seen by an observer from two widely separated points.</p>
+        <br></br>
+        <p id="textblock-content"> Whereas, Parallax scrolling is a web site trend where the background content (i.e. an image) is moved at a different speed than the foreground content while scrolling.
+        </p> 
+        </div>
+        </div>
+    )
+}
+
+export default TextBlock;
+
+
+
+{/* <div id="textblock">
+<div id="textblock-container">
+    <h1 id="textblock-title">Parallax</h1>
+    <p id="textblock-content">
+    parallax, in astronomy, the difference in direction of a celestial object as seen by an observer from two widely separated points.</p>
+</div>
+</div> */}
